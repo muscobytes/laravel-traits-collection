9b57ac65d2a82efd38b38782107c7e6ff796f210
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-28 17:14:26 +0400

    refactor(tests): update namespace for `BaseTest.php`
    - Change namespace from `Muscobytes\Laravel\TraitsCollection\Command` to `Muscobytes\Laravel\TraitsCollection\Tests\Console\Command`
    - Improve code organization by aligning file with proper namespace structure

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Muscobytes\Laravel\TraitsCollection\Command;
+namespace Muscobytes\Laravel\TraitsCollection\Tests\Console\Command;
 
 use PHPUnit\Framework\TestCase;
 
 class BaseTest extends TestCase
 {
-
+    //
 }
