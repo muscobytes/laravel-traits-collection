a71d83f7a9f1546641a365ea845cc64aa5703481
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-28 17:23:20 +0400

    test(BaseTest): add basic test case for `BaseTest`
    - Add `testTrue` method to assert `true` is `true`
    - Import `CoversClass` attribute and apply it to `BaseTest` class

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,9 +2,14 @@
 
 namespace Muscobytes\Laravel\TraitsCollection\Tests\Console\Command;
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(BaseTest::class)]
 class BaseTest extends TestCase
 {
-    //
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
