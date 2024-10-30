2f9d60925c0b84adfc0a105f9cea3c4c5f353054
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-31 00:53:26 +0400

    feat(service-providers): add `PublishesMigrations` trait for migration publishing
    - Introduce `PublishesMigrations` trait to register migrations as publishable assets
    - The trait is designed for package developers to easily manage migrations
    - Utilizes a generator to iterate and rename migration files for publishing

---
+++ src/ServiceProviders/PublishesMigrations.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * PublishesMigrations
+ *
+ * This is modified version of the original
+ * @link https://github.com/DarkGhostHunter/Laratraits
+ *
+ * The following traits is intended for package developers. This will find
+ * all migrations properly named located in the default `database/migrations`
+ * directory, and it will proceed to register each of them as publishable.
+ *
+ *
+ *     public function boot(): void
+ *     {
+ *         $this->publishMigrations();
+ *
+ *         // ...
+ *     }
+ *
+ *
+ */
+
+namespace Muscobytes\Laravel\TraitsCollection\ServiceProviders;
+
+use Generator;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+/**
+ * @property $app
+ * @method publishes(array $array, string $path)
+ */
+trait PublishesMigrations
+{
+    /**
+     * Searches migrations and publishes them as assets.
+     * @see https://darkghosthunter.medium.com/laravel-packages-load-or-publish-migrations-119db770c870
+     *
+     * @param string $directory
+     * @return void
+     * @throws BindingResolutionException
+     */
+    protected function registerMigrations(string $directory): void
+    {
+        if ($this->app->runningInConsole()) {
+            $generator = function(string $directory): Generator {
+                $i = 0;
+                foreach ($this->app->make('files')->allFiles($directory) as $file) {
+                    yield $file->getPathname() => $this->app->databasePath(
+                        preg_replace(
+                            pattern: '/([0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{6})/',
+                            replacement: now()->addSeconds(++$i)->format('Y_m_d_His'),
+                            subject: $file->getFilename()
+                        )
+                    );
+                }
+            };
+
+            $this->publishes(iterator_to_array($generator($directory)), 'migrations');
+        }
+    }
+}
